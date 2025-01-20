23dabf7372b17872b4c9d0b6e6dc791ad60e20de
Alfi Noushad <alfinoushad@gmail.com>
2025-01-20 21:03:08 +0530

    Update Polynomial.c

--- C/Polynomial.c
+++ C/Polynomial.c
@@ -10,3 +10,19 @@ for(i=0;i<=e;i++)
          scanf("%d",&a[i]);
  
      }
+ for(i=e;i>=0;i--)
+     {
+        
+        if(i==0)
+           {
+             printf("%dx^%d",a[i],i);
+           }
+         else
+           {
+                  printf("%dx^%d+",a[i],i);
+           }    
+    
+     
+     }
+return 0;
+}
