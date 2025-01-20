e251c4f17c8d50cf5b82d3ffe5fecdc28f891cda
Alfi Noushad <alfinoushad@gmail.com>
2025-01-20 21:02:55 +0530

    Create Polynomial.c

---
+++ C/Polynomial.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a[10],e,i;
+printf("Enter the highest exponent:");
+scanf("%d",&e);
+for(i=0;i<=e;i++)
+     {
+         printf("Enter the co efficient");
+         scanf("%d",&a[i]);
+ 
+     }
